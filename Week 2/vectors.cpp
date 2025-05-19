886c44f3920c93858c7f591ebf9648f874bb9e81
jsammons3894 <jsammons3894@floridapoly.edu>
2025-05-19 17:24:21 +0000

    hello

---
+++ Week 2/vectors.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+int main () {
+
+    std::vector<int> v {1, 2, 3, 4, 5, 6};
+
+    v.push_back(7);
+
+    std::cout << v.size();
+
+    print(v);
+
+    return 0;
+}
+
+void print(std::vector<int> v) { //range based 'for' loop
+    for(int i : v) {
+std::cout<<i<<' ';
+
+    }
+    std::cout << std:: endl;
+    
+}
+
+void rotateLeft(std::vector<int> v, ink k) {
+    k %= v.size();
+    while (k>0) {
+        int first = v.front(); // returns the first element of a vector
+        v.push_back(first);
+        v.erase(v.begin()); //delets the first element
+    }
+
+}
