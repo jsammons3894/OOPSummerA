e2a6b3c5e701a1f3979354e9543c3aac27b0cce0
Julia Sammons <jsammons3894@floridapoly.edu>
2025-05-20 16:41:53 +0000

    Lets try this

---
+++ Week 2/special_announcement.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+
+bool isVowel(char c) {
+        return c == 'a' || c == 'i' || c = 'o' || c == 'u';
+     }
+
+    string reverseVowels(string &s) {
+        int left = 0
+        int right = s.length()-1;
+
+        while (left<right) {
+
+            while (left < right && !isVowel(s[left])) {
+                left++;
+            }
+
+            while (left < right && !isVowel(s[right])) {
+
+            }
+
+            if (left < right) {
+
+                swap(s[left], s[right]);
+                left++;
+                right--;
+            }
+        }
+
+     }
+
+int main(void) {
+
+    std::string s("90 - 40:sArutciL)2.11 retpihC( sceseB ratneoP ,)7.21
+    ,6.21( sritcoV ,)7 ratpehC( syurri ,)6-ritpohC( snuetcneF ,)5-ratpahC( spaeL ,)4-
+    ritpehC( sgnertS dne sratcurihC:sratpohC kiubtxeT)doksa ab llaw snuetsioq
+    01( .skrim 01 :skreM lateT.setonom 02 raf ,sselC
+    gnarad ,)yedsandiW( 5202/12/50 :ameT & otoD .ssalc yedsundiW roa ne 2 zAaQ aveh et
+    gnoog ire aw ,ridnamer kceuq e");
+
+    std::reverse(s.begin(), s.end());
+    std::cout << s << endl;
+    
+
+//reverseVowels(s);
+//reverse(s);
+std::cout << s << std::endl;
+return 0;
+}
