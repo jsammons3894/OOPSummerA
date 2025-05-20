e82cf7a1301707c352b5eaf2278595133a6ee0cf
Julia Sammons <jsammons3894@floridapoly.edu>
2025-05-20 16:59:28 +0000

    fixing compiling errors

--- Week 2/special_announcement.cpp
+++ Week 2/special_announcement.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 bool isVowel(char c) {
-        return c == 'a' || c == 'i' || c = 'o' || c == 'u';
+        return c == 'a' || c == 'i' || c == 'o' || c == 'u';
      }
 
     string reverseVowels(string &s) {
-        int left = 0
+        int left = 0;
         int right = s.length()-1;
 
         while (left<right) {
@@ -16,7 +17,7 @@ bool isVowel(char c) {
             }
 
             while (left < right && !isVowel(s[right])) {
-
+                right --;
             }
 
             if (left < right) {
@@ -26,12 +27,12 @@ bool isVowel(char c) {
                 right--;
             }
         }
-
+        return s;
      }
 
 int main(void) {
 
-    std::string s("90 - 40:sArutciL)2.11 retpihC( sceseB ratneoP ,)7.21
+    std::string s = ("90 - 40:sArutciL)2.11 retpihC( sceseB ratneoP ,)7.21
     ,6.21( sritcoV ,)7 ratpehC( syurri ,)6-ritpohC( snuetcneF ,)5-ratpahC( spaeL ,)4-
     ritpehC( sgnertS dne sratcurihC:sratpohC kiubtxeT)doksa ab llaw snuetsioq
     01( .skrim 01 :skreM lateT.setonom 02 raf ,sselC
@@ -39,11 +40,7 @@ int main(void) {
     gnoog ire aw ,ridnamer kceuq e");
 
     std::reverse(s.begin(), s.end());
-    std::cout << s << endl;
-    
 
-//reverseVowels(s);
-//reverse(s);
 std::cout << s << std::endl;
 return 0;
 }
