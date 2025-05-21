1021778e8bad686b554044a794095ea20ea05b7e
Julia Sammons <jsammons3894@floridapoly.edu>
2025-05-21 16:51:37 +0000

    help

---
+++ Week 2/attend5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+void rotate_left(std::string& src, int nRotations){
+    for (int i, i < n_rotations, i++);{
+    s.insert(s.begin(), s[s.size()-1]);
+    s.popback();
+}
+}
+
+int main(void) {
+std::string s("1u34g6rjyjddfhsdf5adh6jsdfj1sldj4kao9sjdf");
+int n_rotations = pow((100 * 2 - 75 / 3 + 20 % 7 + 20 + 12 / 4), 3);
+rotate_left(s, n_rotations);
+std::string attend(s, 5, 5);
+std::cout << attend << std::endl;
+return 0;
+}
+
+void s(std::string& src, int nRotations) {
+}
