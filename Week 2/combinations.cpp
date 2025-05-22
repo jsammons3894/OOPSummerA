644b61da35a287eee9beb0f28369e889a91be5e2
Julia Sammons <jsammons3894@floridapoly.edu>
2025-05-22 16:47:10 +0000

    k fdji

--- Week 2/combinations.cpp
+++ Week 2/combinations.cpp
@@ -7,10 +7,8 @@ void comb(int n_chars);
 
 void comb_helper(std::String& s, int n_chars, int i);
 
-
-
 int main () {
-
+    comb(3);
     return 0;
 }
 
@@ -19,4 +17,17 @@ void comb(int n_chars){
     std::String s (n_chars, 'A');
     comb_helper(s, n_chars, 0);
     }
+
+void comb_helper(std::string& s, int n_chars, int i) {
+    if (i >= n_chars) {
+     return;
+    }
+    else {
+        while (s[i] <= 'Z') {
+            comb_helper(s, n_chars, i+1);
+        s[i]++;
+    }
+}
+
+s[i] = 'A';
 }
