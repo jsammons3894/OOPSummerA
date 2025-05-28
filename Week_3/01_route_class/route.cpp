c603f56f6ee4a3ba096e15481bd81e4f3644a4bc
Julia Sammons <jsammons3894@floridapoly.edu>
2025-05-28 17:03:05 +0000

    pre-length

--- Week_3/01_route_class/route.cpp
+++ Week_3/01_route_class/route.cpp
@@ -1,26 +1,52 @@
 #include <iostream>
 #include <string>
 
-struct route {
+class Route { //class class class!!!
 
-std::string source;
-std::string destination;
+
+private: //acess specifier... // When we have this complicated logic, it is no longer a structure- now its a class!
+    std::string source;
+    std::string destination;
+    int length;
+
+public:
+    //get functions (Acessor functions, getters)
+    std::string getSource(){
+        return source;
+    }
+    std::string getDestination(){
+        return destination;
+    }
+    int getLength(){
+        return length;
+    }
+
+    // set functions (mutator function, setters)
+
+    void setSource(const std::string& new_source){
+        source = new_source;
+    }
+    void setDestination(const std::string& new_destination){
+        destination = new_destination;
+    }
+    //not length because we are doing this seperately
 
 };
 
 
-void print_route(const struct route&){
+void printRoute(Route& route){
 
-std::cout << route.source << "->" << route.destination << std::endl;
+std::cout << route.getSource() << " -> " << route.getDestination() << ", " << route.getLength() << std::endl;
+
 
 }
 
 int main () {
 
-    struct route summer_trip;
-    summer_trip.source = "Lakeland";
-    summer_trip.destination = "Chicago";
-
-    printroute(summer_trip);
+    Route summer_trip;
+    summer_trip.setSource("Lakeland");
+    summer_trip.setDestination("Chicago");
+    //summer_trip.length = 800;
+    printRoute(summer_trip);
 
 }
