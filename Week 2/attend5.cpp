f6ac019c60fbb7d28563545eb76a17560a33e755
Julia Sammons <jsammons3894@floridapoly.edu>
2025-05-28 12:28:38 -0400

    k

--- Week 2/attend5.cpp
+++ Week 2/attend5.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <iostream>
+#include <string>
+#include <cmath>
 void rotate_left(std::string& src, int nRotations){
     for (int i, i < n_rotations, i++);{
     s.insert(s.begin(), s[s.size()-1]);
@@ -8,6 +11,21 @@ void rotate_left(std::string& src, int nRotations){
 }
 }
 
+<<<<<<< HEAD
+void rotate_left(std::string& src, int nRotations);
+
+int main(void) {
+std::string long_string("1u34g6rjyjddfhsdf5adh6jsdfj1sldj4kao9sjdf");
+int n_rotations = pow((100 * 2 - 75 / 3 + 20 % 7 + 20 + 12 / 4), 3);
+rotate_left(long_string, n_rotations);
+std::string attend(long_string, 5, 5);
+std::cout << attend << std::endl;
+return 0;
+}
+
+void rotate_left(std::string& src, int nRotations) {
+}
+=======
 int main(void) {
 std::string s("1u34g6rjyjddfhsdf5adh6jsdfj1sldj4kao9sjdf");
 int n_rotations = pow((100 * 2 - 75 / 3 + 20 % 7 + 20 + 12 / 4), 3);
@@ -19,3 +37,4 @@ return 0;
 
 void s(std::string& src, int nRotations) {
 }
+>>>>>>> d4dcf5e3c80cd965aa15baa141edf16b78ee8f05
