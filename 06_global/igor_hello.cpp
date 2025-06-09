3b148c84fe94b241e2185cbdebeea676920c6875
Julia Sammons <jsammons3894@floridapoly.edu>
2025-06-09 16:35:13 +0000

    balls

--- 06_global/igor_hello.cpp
+++ 06_global/igor_hello.cpp
@@ -2,80 +2,72 @@
 #include <string>
 class Hello {
 public:
-    static int numberOfObjects;
-
-    Hello() : messages(nullptr), size(0) {
-    std::cout << "Hi!" << std::endl;
-    numberOfObjects++;
-    }
-    Hello(int n) : size(n) {
+static int numberOfObjects;
+Hello() : messages(nullptr), size(0) {
+std::cout << "Hi!" << std::endl;
+numberOfObjects++;
+}
+Hello(int n) : size(n) {
 // int i = 0;
 // while (i < n) {
 // std::cout << "Hi!" << std::endl;
 // i++;
 // }
 // Dynamically allocate an array of strings
-    messages = new std::string[n];
-
+messages = new std::string[n];
 // Initialize the array
-    for(int i = 0; i < n; i++) {
-        if (i % 3 == 0) {
-            messages[i] = "Hi!";
-        }
-    else if (i % 3 == 1) {
-        messages[i] = "Hello!";
-    }
-    else {
-        messages[i] = "Welcome!";
-    }
-    }
-    numberOfObjects++;
+for(int i = 0; i < n; i++) {
+if (i % 3 == 0) {
+messages[i] = "Hi!";
 }
-
+else if (i % 3 == 1) {
+messages[i] = "Hello!";
+}
+else {
+messages[i] = "Welcome!";
+}
+}
+numberOfObjects++;
+}
 //Copy constructor
-
-Hello(const Hello& other){
-    std::cout << "The copy constructor was called" <<std::endl;
-    size = size.other;
-
+Hello(const Hello& other) {
+std::cout << "The copy constructor was called" << std::endl;
+size = other.size;
+messages = new std::string[size];
+//copy the values
+for (int i = 0; i < size; i++) {
+messages[i] = other.messages[i];
 }
-
-//Destructor
+numberOfObjects++;
+}
+// Destructor
 ~Hello() {
-    //When the object should be destroyed
-    std::cout << "The Destructor was called." << std::endl;
-    delete [] messages;
-    numberOfObjects--;
+// std::cout << "The destructor was called" << std::endl;
+delete [] messages;
+numberOfObjects--;
 }
-
 // Method
 void bye() const {
 std::cout << "Bye!" << std::endl;
 }
-
 void printGreetings() const {
 for (int i = 0; i < size; i++) {
 std::cout << messages[i] << std::endl;
 }
 }
-
-
 private:
 std::string* messages; // array of greetings
 int size;
 };
-
-void test(){
-    Hello h(999999);
+void test() {
+// std::cout << "The function starts" << std::endl;
+Hello h(999999);
+// std::cout << "The function ends" << std::endl;
 }
-
-void test2(Hello h){
-
+void test2(Hello h) {
 }
-
-//initalize the static variable
+// Initialize the static data member of the class
 int Hello::numberOfObjects = 0;
-
 int main() {
 // Create an object of Hello class dynamically
 Hello* hi = new Hello;
@@ -86,26 +78,20 @@ Hello* hi2 = new Hello(10);
 hi2->bye();
 // Print greetings
 hi2->printGreetings();
-
+// Print the number of objects
+std::cout << Hello::numberOfObjects << std::endl;
+// Deallocate dynamic memory
 delete hi;
 delete hi2;
 for (int i = 0; i < 10; i++) {
-//Hello hello(999999999);
+// Hello hello(999999999);
 test();
 Hello h;
 test2(h);
 }
-
-//print the numebr of objects
-
-
 std::cout << Hello::numberOfObjects << std::endl;
-// Deallocate dynamic memory
-
-
-//call a copy constructore
-Hello hi;
-Hello h2(hi);
-
+// Call a copy constructor
+Hello h1;
+Hello h2(h1);
 return 0;
 }
