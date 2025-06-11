35322f3d53c0ca481ca1b246b1d73db58a243036
Julia Sammons <jsammons3894@floridapoly.edu>
2025-06-11 12:13:39 -0400

    iug

--- Week_3/01_route_class/route.cpp
+++ Week_3/01_route_class/route.cpp
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 #include <iostream>
 #include <string>
-=======
-#include <iostream>
-#include <string>
-#include <cstdlib>
-#include <ctime>
->>>>>>> 4627ab3870fc43f4d1dd69ac44d5622cab76b40d
+
 
 struct route {
 
@@ -31,3 +25,30 @@ int main () {
     printroute(summer_trip);
 
 }
+
+=======
+struct route {
+
+std::string source;
+std::string destination;
+
+};
+
+
+void print_route(const struct route&){
+
+std::cout << route.source << "->" << route.destination << std::endl;
+
+}
+
+int main () {
+
+    struct route summer_trip;
+    summer_trip.source = "Lakeland";
+    summer_trip.destination = "Chicago";
+
+    printroute(summer_trip);
+
+}
+
+>>>>>>> f7170eea516dbad92c5a6573ef59ad230aeb61a2
