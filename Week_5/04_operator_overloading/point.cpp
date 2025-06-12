4bd4ca79306aebed7431ccf4052b0a7a591ce018
Julia Sammons <jsammons3894@floridapoly.edu>
2025-06-12 16:19:05 +0000

    lkgrjkrjb

---
+++ Week_5/04_operator_overloading/point.cpp
@@ -0,0 +1,8 @@
+#include "point.h"
+
+//implement constructor
+
+Point::Point(int x, int y) {
+    this->x = x;
+    this->y = y;
+}
---
+++ Week_5/04_operator_overloading/point.h
@@ -0,0 +1,17 @@
+#ifndef POINT_H
+#define POINT_H
+
+class Point {
+    public:
+
+    //constructor with default values
+    Point (int x = 0, int y = 0);
+    
+
+    private:
+        int x,y;
+};
+
+
+
+#endif
