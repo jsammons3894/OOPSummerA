629fbdc9224795e0838b59450ecc523bb8d2cc0f
Julia Sammons <jsammons3894@floridapoly.edu>
2025-06-12 16:21:33 +0000

    hwllo?

--- Week_5/04_operator_overloading/point.cpp
+++ Week_5/04_operator_overloading/point.cpp
@@ -1,8 +1,110 @@
 #include "point.h"
-
+#include <string>
+#include <cstring>
 //implement constructor
 
-Point::Point(int x, int y) {
+Point::Point(int x, int y, const std::string& tag) {
     this->x = x;
     this->y = y;
+    this->tag = new char[tag.size() + 1];
+    //copy the characters from the string
+    int i;
+    for (int i = 0; i < tag.size(); i++){
+        this->tag[i] = tag[i];
+    }
+    this ->tag[i] = '\0';
 }
+
+Point::~Point() {
+    delete [] tag;
+}
+
+Point::Point(const Point& other) {
+    x = other.x;
+    y = other.y;
+    tag = new char [strlen(other.tag)];
+    tag = new char[size+1];
+    for (int i = 0; i < size; i++) {
+        tag [i] = other.tag[i];
+    }
+    tag[size] = '\0';
+}
+
+std::string Point::toString() const{
+    return "(" + std::to_string(x) + ", " + std::to_string(y) + ")";
+}
+
+ bool Point::operator==(const Point& other){
+    return (x == other.x && y == other.y);
+}
+
+bool Point::operator!=(const Point& other){
+    //return (x != other.x || y != other.y);
+    //OR
+   // return !(x == other.x && y == other.y);
+   //OR
+   return !(*this == other);
+}
+
+int& Point::operator[](int index){
+    if (index == 0) {
+        return x;
+    }
+    else {
+        return y;
+    }
+}
+ 
+Point Point::operator+(const Point& other) {
+    //int m = x + other.x;
+    //int n = y + other.y;
+    //OR
+    return Point(x + other.x, y + other.y); 
+}
+
+Point Point::operator+=(const Point& other) {
+    this->x += other.x;  // Add the x-coordinates
+    this->y += other.y;  // Add the y-coordinates
+    return *this;       // Return a reference to the modified object
+}
+
+
+Point Point::operator++(){
+    x++;
+    y++;
+
+    return *this; //return the current thing
+}
+
+Point Point::operator++(int ){ 
+    Point temp(x, y);
+    x++;
+    y++;
+    return temp;
+}
+
+Point Point::operator*(const Point& other) {
+    int m = x * other.x;
+    int n = y * other.y;
+    return Point(m, n);
+}
+
+Point Point::operator--() {
+x--;
+y--;
+return *this;
+}
+
+Point Point::operator--(int ){
+Point temp (x, y);
+x--;
+y--;
+return temp;
+}
+
+Point Point::operator*=(const Point& other){
+ int m = x * other.y;
+ int n = y * other.x;
+ return Point(m,n);
+}
+
--- Week_5/04_operator_overloading/point.h
+++ Week_5/04_operator_overloading/point.h
@@ -1,17 +1,53 @@
 #ifndef POINT_H
 #define POINT_H
-
+#include <string>
+#include <iostream>
 class Point {
     public:
 
     //constructor with default values
     Point (int x = 0, int y = 0);
-    
+
+    //destructor
+    ~Point();
+
+    //copy constructor
+
+    Point(const Point& other);
+
+    std::string toString() const;
+
+    bool operator==(const Point& other);
+    bool operator!=(const Point& other);
+
+    //Operator brackets []
+
+    int& operator[](int index);
+
+    //Arethemetic operators
+    Point operator+(const Point& other);
+
+    Point operator*(const Point& other);
+
+    //Arethemetic assignment operator
+
+    Point operator+=(const Point& other);
+
+
+    //preincrement
+    Point operator++();
+
+    //postincrement
+    Point operator++(int );
+
+
+    Point operator--();
+    Point operator--(int );
+    Point operator*=(const Point& other);
+
 
     private:
-        int x,y;
+        int x, y;
 };
-
-
 
 #endif
