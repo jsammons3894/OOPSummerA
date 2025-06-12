f733604f1da8d576f965682ecfc542b1f6fd767f
Julia Sammons <jsammons3894@floridapoly.edu>
2025-06-12 17:36:21 +0000

    HEEEELP

---
+++ Week_5/04_operator_overloading/point3D.cpp
@@ -0,0 +1,10 @@
+#include "Point3D.h"
+
+Point3D::Point3D(int x, int y, int z, const string& tag) : Point(x, y, tag){
+    this->z = z;
+}
+
+void print(){
+    std::cout << tag << std::endl;
+    std::cout << x << ", " << y << ", " << z << std::endl;
+}
--- Week_5/04_operator_overloading/testpoint.cpp
+++ Week_5/04_operator_overloading/testpoint.cpp
@@ -1,35 +1,49 @@
 #include "Point.h"
 #include <iostream>
 int main() {
-Point p1(3, 4);
-Point p2(5, 4);
-Point p3(89, 55);
-std::cout << p1.toString() << std::endl;
-std::cout << (p1 == p2) << std::endl;
-if (p1 != p2) {
-std::cout << (char) p3[0] << std::endl;
-}
-else {
-std::cout << (char) p3[1] << std::endl;
-}
-p1[0] = 77;
-std::cout << (char) p1[0];
-p1[0] = p1[0] - 59;
-p2[0] = p2[0] + 3;
-Point p4 = p1 + p2;
-std::cout << (char) (p4[0] - 56);
-Point p5(60, 74);
-p5 += p1;
-std::cout << (char) (p5[1] - 4);
-// p5 *= p1;
-// for (int i = 0; i < 119; i++) {
-// p5--;
-// --p5;
+Point p1(3, 4, "fisrt");
+Point3D p2(3, 4, 5, "second");
+
+p1.print();
+p2.print();
+
+
+
+
+
+
+
+
+
+
+// Point p2(5, 4);
+// Point p3(89, 55);
+// std::cout << p1.toString() << std::endl;
+// std::cout << (p1 == p2) << std::endl;
+// if (p1 != p2) {
+// std::cout << (char) p3[0] << std::endl;
 // }
-std::cout << (char) p5[1];
-Point p6(0, 0, "Center");
-p6.print();
-Point p7 = p1 = p3;
-p7.print();
+// else {
+// std::cout << (char) p3[1] << std::endl;
+// }
+// p1[0] = 77;
+// std::cout << (char) p1[0];
+// p1[0] = p1[0] - 59;
+// p2[0] = p2[0] + 3;
+// Point p4 = p1 + p2;
+// std::cout << (char) (p4[0] - 56);
+// Point p5(60, 74);
+// p5 += p1;
+// std::cout << (char) (p5[1] - 4);
+// // p5 *= p1;
+// // for (int i = 0; i < 119; i++) {
+// // p5--;
+// // --p5;
+// // }
+// std::cout << (char) p5[1];
+// Point p6(0, 0, "Center");
+// p6.print();
+// Point p7 = p1 = p3;
+// p7.print();
 return 0;
 }
